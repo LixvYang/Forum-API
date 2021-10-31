efce5bca28fb575887794c82329a3de0e36aa683
lixin yang <2690688423@qq.com>
2021-10-31 10:40:23 +0800

    update sql

--- db/db.sql
+++ db/db.sql
@@ -19,7 +19,9 @@ CREATE TABLE `user`
   `deleted_at` datetime(3) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-INSERT INTO `user`  VALUES (1, 'admin', '$2a$10$K3DRY94amW.0Shce.5FsmuDqjKPzQczll.s6fHA361yPJ0le1Igr.','https://user-gold-cdn.xitu.io/2019/5/29/16b028263cf8b532?imageView2/1/w/100/h/100/q/85/format/webp/interlace/1','2021-10-22 17:05:14.764', NULL , NULL);
+INSERT INTO `user`  VALUES (1, 'admin', '$2a$10$K3DRY94amW.0Shce.5FsmuDqjKPzQczll.s6fHA361yPJ0le1Igr.','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmb_j0g0GkHwuF42L8gNpJtSrT10kRQyEMvQ&usqp=CAU','2021-10-22 17:05:14.764', NULL , NULL);
+INSERT INTO `user`  VALUES (2, 'civy', '$2a$10$K3DRY94amW.0Shce.5FsmuDqjKPzQczll.s6fHA361yPJ0le1Igr.','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmb_j0g0GkHwuF42L8gNpJtSrT10kRQyEMvQ&usqp=CAU','2021-10-24 17:05:14.764', NULL , NULL);
+
 
 -- ----------------------------
 -- Table structure for role
@@ -38,6 +40,8 @@ CREATE TABLE `role`
 -- Records of role
 -- ----------------------------
 INSERT INTO `role` VALUES (1,'admin','2021-10-22 18:25:33.763', NULL , NULL);
+INSERT INTO `role` VALUES (2,'user','2021-10-22 18:25:33.763', NULL , NULL);
+
 
 -- ----------------------------
 -- Table structure for user_role
@@ -58,6 +62,8 @@ CREATE TABLE `user_role`
 -- Records of user_role
 -- ----------------------------
 INSERT INTO `user_role`  VALUES (1, 1, 1,'2021-10-22 18:25:33.763', NULL , NULL);
+INSERT INTO `user_role`  VALUES (1, 2, 2,'2021-10-22 18:25:33.763', NULL , NULL);
+
 
 DROP TABLE IF EXISTS `menu`;
 CREATE TABLE `menu`
@@ -72,21 +78,20 @@ CREATE TABLE `menu`
   PRIMARY KEY (`id`) USING BTREE
 );
 INSERT INTO `menu` (id,name,path,method,created_at,updated_at,deleted_at) VALUES
-(1,'获取用户信息','/v1/auth/info','GET','2021-10-22 18:29:33.763', NULL , NULL),
+(1,'获取用户信息','/v1/auth/userinfo','GET','2021-10-22 18:29:33.763', NULL , NULL),
 (2, '获取用户列表', '/v1/user', 'GET', '2021-10-22 18:29:33.763', NULL , NULL),
 (3, '获取单个用户', '/v1/user/:id', 'GET', '2021-10-22 18:29:33.763', NULL , NULL),
-(4, '创建用户', '/v1/user/', 'POST', '2021-10-22 18:29:33.763', NULL , NULL),
+(4, '创建用户', '/v1/user/add', 'POST', '2021-10-22 18:29:33.763', NULL , NULL),
 (5, '删除用户', '/v1/user/:id', 'DELETE', '2021-10-22 18:29:33.763', NULL , NULL),
 (6, '修改用户', '/v1/user/:id', 'PUT', '2021-10-22 18:29:33.763', NULL , NULL),
-(7, '查询所有菜单', '/v1/menus', 'GET', '2021-10-22 18:29:33.763', NULL , NULL),
-(8, '查询单个菜单', '/v1/menus/:id', 'GET', '2021-10-22 18:29:33.763', NULL , NULL),
-(9, '创建单个菜单', '/v1/menus', 'POST', '2021-10-22 18:29:33.763', NULL , NULL),
-(10, '更新单个菜单', '/v1/menus/:id', 'PUT','2021-10-22 18:29:33.763', NULL , NULL),
-(11, '删除单个菜单', '/v1/menus/:id', 'DELETE','2021-10-22 18:29:33.763', NULL , NULL),
-
-(12, '查询所有角色', '/v1/roles', 'GET','2021-10-22 18:29:33.763', NULL , NULL),
-(13, '查询单个角色', '/v1/roles/:id', 'GET','2021-10-22 18:29:33.763', NULL , NULL),
-(14, '创建单个角色', '/v1/roles', 'POST','2021-10-22 18:29:33.763', NULL , NULL),
+(7, '查询所有菜单', '/v1/menu', 'GET', '2021-10-22 18:29:33.763', NULL , NULL),
+(8, '查询单个菜单', '/v1/menu/:id', 'GET', '2021-10-22 18:29:33.763', NULL , NULL),
+(9, '创建单个菜单', '/v1/menu/add', 'POST', '2021-10-22 18:29:33.763', NULL , NULL),
+(10, '更新单个菜单', '/v1/menu/:id', 'PUT','2021-10-22 18:29:33.763', NULL , NULL),
+(11, '删除单个菜单', '/v1/menu/:id', 'DELETE','2021-10-22 18:29:33.763', NULL , NULL),
+(12, '查询所有角色', '/v1/role', 'GET','2021-10-22 18:29:33.763', NULL , NULL),
+(13, '查询单个角色', '/v1/role/:id', 'GET','2021-10-22 18:29:33.763', NULL , NULL),
+(14, '创建单个角色', '/v1/role/add', 'POST','2021-10-22 18:29:33.763', NULL , NULL),
 (15, '更新单个角色', '/v1/roles/:id', 'PUT','2021-10-22 18:29:33.763', NULL , NULL),
 (16, '删除单个角色', '/v1/roles/:id', 'DELETE','2021-10-22 18:29:33.763', NULL , NULL);
 
@@ -131,7 +136,8 @@ CREATE TABLE `article`
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 INSERT INTO `article` VALUES (1,'Hello','Linux',1,1,1,'2021-10-22 18:29:33.763',NULL,NULL);
-INSERT INTO `article` VALUES (2,'Xin','Long for run.',1,1,1,'2021-10-22 18:29:33.763',NULL,NULL);
+INSERT INTO `article` VALUES (2,' Be Patience','Stock for the long run.',1,1,1,'2021-10-22 18:29:33.763',NULL,NULL);
+INSERT INTO `article` VALUES (3,' Keep Learning','The best way to keep learning.',1,1,1,'2021-10-22 18:29:33.763',NULL,NULL);
 
 DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category`
@@ -143,7 +149,10 @@ CREATE TABLE `category`
   `deleted_at`    datetime(3) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 );
-INSERT INTO `category` VALUES (1,'Learn','2021-10-22 18:29:33.763',NULL,NULL);
+INSERT INTO `category` VALUES (1,'Technology','2021-10-22 18:29:33.763',NULL,NULL);
+INSERT INTO `category` VALUES (2,'Humanities','2021-10-22 18:29:33.763',NULL,NULL);
+INSERT INTO `category` VALUES (3,'Politics','2021-10-22 18:29:33.763',NULL,NULL);
+INSERT INTO `category` VALUES (4,'Language','2021-10-22 18:29:33.763',NULL,NULL);
 
 DROP TABLE IF EXISTS `tag`;
 CREATE TABLE `tag`
@@ -155,4 +164,9 @@ CREATE TABLE `tag`
   `deleted_at` timestamp NULL,
   PRIMARY KEY (`id`) USING BTREE
 );
-INSERT INTO `tag` VALUES  (1,'Tip','2021-10-22 18:29:33.763',NULL,NULL);
+INSERT INTO `tag` VALUES  (1,'Learning Tip','2021-10-22 18:29:33.763',NULL,NULL);
+INSERT INTO `tag` VALUES  (2,'Make','2021-10-22 18:29:33.763',NULL,NULL);
+INSERT INTO `tag` VALUES  (3,'CryptoCurrency','2021-10-22 18:29:33.763',NULL,NULL);
+INSERT INTO `tag` VALUES  (4,'DigitalCurrency','2021-10-22 18:29:33.763',NULL,NULL);
+
+
